ba1b494b114dede56966b1fdae6eafa19d2beab0
“shengming-ma” <1913299123@qq.com>
2025-01-07 00:23:42 +0800

    first

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    <h2><NuxtLink to="login">to login</NuxtLink></h2>
+    <h2><NuxtLink to="about">to about</NuxtLink></h2>
+   
+    
+  </header>
+  <div>
+   <NuxtPage />
+  </div>
+</template>
